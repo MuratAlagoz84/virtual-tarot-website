f73d7e568eb00c07946938796e2754cef0e108cc
Murat Alagöz <53307241+MuratAlagoz84@users.noreply.github.com>
2025-08-07 21:49:45 +0300

    fix: Remove trailingSlash to resolve build conflict

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://virtualtarotapp.com',
-  
+  // Sitemap oluşturmak için bu ayar şart
+  site: 'https://www.virtualtarotapp.com', 
+
+  // i18n (çoklu dil) ayarları
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'tr', 'es', 'fr', 'de', 'it', 'pt', 'ru', 'ja', 'zh', 'ar'],
